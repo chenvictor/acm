d4243a6f54ca3052cbc51ef9024c731e0e3d358e
Victor Chen <victor.chen@alumni.ubc.ca>
2021-01-06 17:42:39 -0800

    docs for aho

--- strings/aho.cpp
+++ strings/aho.cpp
@@ -1,64 +1,77 @@
-//https://github.com/kth-competitive-programming/kactl/blob/master/content/strings/AhoCorasick.h
-struct Aho {
-  static const int ALPHA = 26;
-    static int toi(char c) {
-    return c - 'a';
-  }
+// https://github.com/kth-competitive-programming/kactl/blob/master/content/strings/AhoCorasick.h
+// 1. Aho::insert() strings
+// 2. Aho::build()
+// findAll type operations NsqrtN ONLY IF STRINGS DISTINCT
+namespace Aho {
+  const int ALPHA = 26;
+  int toi(char c) { return c - 'a'; }
   struct Node {
-    int back, next[ALPHA], start = -1, end = -1, nmatches = 0;
-    Node(int v) { memset(next, v, sizeof(next)); }
+    int back, next[ALPHA], s = -1, end = -1, cnt = 0;
+    // back is suffix link
+    // next is node next node given char
+    // end is longest match ending at current position
+    // cnt is number of matching ending at current position
+    Node(int v) { fill(next, next+ALPHA, v); }
   };
-  int sz = 0;
-  vector<Node> N = {-1}; VI backp;
-  int insert(string& s) {
+  int sz = 0; vector<Node> N = {-1}; vi backp;
+  // backp[i] is the longest suffix of string i, -1 if none
+  // returns terminal node of added string
+  int insert(const string& s) {
     assert(!s.empty());
-    int j = sz++;
     int n = 0;
     for (char c : s) {
       int& m = N[n].next[toi(c)];
       if (m == -1) { n = m = N.size(); N.emplace_back(-1); }
       else n = m;
     }
-    if (N[n].end == -1) N[n].start = j;
+    if (N[n].end == -1) N[n].s = sz;
     backp.push_back(N[n].end);
-    N[n].end = j;
-    N[n].nmatches++;
+    N[n].end = sz++;
+    N[n].cnt++;
     return n;
   }
   void build() {
     N[0].back = N.size();
     N.emplace_back(0);
-    queue<int> q;
-    for (q.push(0); !q.empty(); q.pop()) {
+    for (queue<int> q({0}); q.size(); q.pop()) {
       int n = q.front(), prev = N[n].back;
-      forx(i,ALPHA) {
+      rep(i,0,ALPHA) {
         int &ed = N[n].next[i], y = N[prev].next[i];
-        if (ed == -1) ed = y;
-        else {
+        if (ed == -1) {
+          ed = y;
+        } else {
           N[ed].back = y;
-          (N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
+          (N[ed].end == -1 ? N[ed].end : backp[N[ed].s])
             = N[y].end;
-          N[ed].nmatches += N[y].nmatches;
+          N[ed].cnt += N[y].cnt;
           q.push(ed);
         }
       }
     }
     N[0].back = 0; N.pop_back();
   }
-  VI find(string word) {
-    int n = 0;
-    VI res; // ll count = 0;
+  // nicer interface
+  const struct Iter {
+    int id; Iter(int i=0): id(i) {}
+    Iter go(char c) { return N[id].next[toi(c)]; }
+    Node& operator()() { return N[id]; }
+  } root;
+  // returns longest match ending at each pos
+  vi find(string word) {
+    Iter i = root;
+    vi res; // ll count = 0;
     for(char c : word) {
-      n = N[n].next[c - first];
-      res.push_back(N[n].end);
-      // count += N[n].nmatches;
+      i = i.go(c);
+      res.push_back(i().end);
+      // count += N[n].cnt;
     }
     return res;
   }
-  vector<VI> findAll(vector<string>& pat, string word) {
-    VI r = find(word);
-    vector<VI> res(word.size());
-    forx(i,word.size()) {
+  // returns all matches starting at each pos, shortest first
+  vector<vi> findAll(vector<string>& pat, string word) {
+    vi r = find(word);
+    vector<vi> res(word.size());
+    rep(i,0,word.size()) {
       int ind = r[i];
       while (ind != -1) {
         res[i - pat[ind].size() + 1].push_back(ind);
