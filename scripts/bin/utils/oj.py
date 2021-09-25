b88579268ab2481b969b91b9a07cfa27927cc7e1
BuildTools <victor.chen@alumni.ubc.ca>
2021-09-25 00:58:46 -0700

    atcoder working?

--- scripts/bin/utils/oj.py
+++ scripts/bin/utils/oj.py
@@ -1,17 +1,48 @@
-from abc import ABC, abstractmethod
+from argparse import ArgumentParser, FileType
+from abc import ABCMeta, abstractmethod
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 from robobrowser import RoboBrowser
-from abc import ABCMeta, abstractmethod
 import json
 from pathlib import Path
+from typing import Optional
+import sys
+
+def error(msg):
+    print(msg, file=sys.stderr)
+    exit(-1)
+
+DATA_FILE = '.data'
+
+def load_data():
+    try:
+        with open(DATA_FILE, 'r') as f:
+            return json.load(f)
+    except:
+        error('failed to load {}\nrun init first?'.format(DATA_FILE))
+
+def save_data(data):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(data, f)
+
 
 class OnlineJudge(metaclass=ABCMeta):
 
     def __init__(self):
-        creds = self.get_creds()
         self.robo = RoboBrowser(parser='html.parser')
-        self.login(creds['username'], creds['token'])
+        parser = ArgumentParser()
+        subparsers = parser.add_subparsers(dest='cmd', required=True)
+        parser_init = subparsers.add_parser('init', aliases=['i'])
+        parser_init.add_argument('contest')
+        parser_init.set_defaults(func=self.init)
+
+        parser_submit = subparsers.add_parser('submit', aliases=['sm'])
+        parser_submit.add_argument('source', type=FileType('r'))
+        parser_submit.add_argument('--problem', '-p', required=False)
+        parser_submit.set_defaults(func=self.submit)
+
+        args = parser.parse_args()
+        args.func(args)
 
     @property
     @abstractmethod
@@ -27,40 +58,50 @@ class OnlineJudge(metaclass=ABCMeta):
             with open(path, 'r') as f:
                 creds = json.load(f)
         except:
-            print('failed to read config.json!')
-            exit(-1)
+            error('failed to read config.json!')
         for key in ['username', 'token']:
             if key not in creds:
-                print('{} is missing {}'.format(self.config, key))
-                exit(-1)
+                error('{} is missing {}'.format(self.config, key))
         return creds
 
     @abstractmethod
-    def login(self, user, token) -> None:
+    def do_login(self, user, token) -> Optional[str]:
+        """
+        Login to the online judge
+        Return error message, or None if success
+        """
         pass
+
+    def login(self):
+        creds = self.get_creds()
+        res = self.do_login(creds['username'], creds['token'])
+        if res is not None:
+            error(res)
 
     # Return error message, or None if success
     @abstractmethod
-    def submit(self, contestId, problemId, lang, sourceFile):
+    def do_submit(self, contestId, problemId, lang, sourceFile) -> Optional[str]:
+        """
+        Submit to the online judge
+        Return error message, or None if success
+        """
         pass
 
-    @abstractmethod
-    def get_samples(self, contestId, problemId):
-        pass
+    def init(self, args):
+        contest = args.contest
+        save_data({
+            'contest': contest
+        })
 
-    # Return (message, done) pair
-    def ping(self, contestId, problemId):
-        logger.error('Watch not supported!')
+    def submit(self, args):
+        self.login()
+        data = load_data()
+        filename = Path(args.source.name)
 
-#    def watch(self, contestId, problemId):
-#        import spinner
-#        from time import sleep
-#        ping_func = self.ping(contestId, problemId)
-#
-#        while True:
-#            status, done = next(ping_func)
-#            spinner.display(status)
-#            if done:
-#                break
-#            sleep(TICK)
+        problem = args.problem or filename.stem
+        lang = filename.suffix
+        res = self.do_submit(data['contest'], problem, lang, args.source)
+        if res is not None:
+            error(res)
+        print('submitted', file=sys.stderr)
 
