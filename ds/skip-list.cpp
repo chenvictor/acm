69c56dac522b06ec581f2d749ac31416d404f0aa
cvic <chenvictor1999@gmail.com>
2021-09-27 23:14:26 -0700

    blehh

---
+++ ds/skip-list.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,s,e) for(int i = s; i < (e); ++i)
+using ll = long long; using ld = long double;
+using pii = pair<int,int>; using pll = pair<ll, ll>;
+using vi = vector<int>; using vl = vector<ll>;
+const char nl = '\n';
+const ll LINF = 0x3f3f3f3f3f3f3f3fll; const int INF = (int)LINF;
+const ld EPS = 1e-9, PI = acos(-1.0L);
+#define ff first
+#define ss second
+#define all(c) begin(c),end(c)
+#define sz(c) int(c.size())
+
+/* BOOK CODE */
+
+template <typename K, typename V, class Compare=less<T>>
+struct skiplist {
+  list<
+};
+
+/* REAL CODE */
+
+void code() {
+  
+}
+
+/* MAIN CODE */
+
+int main() { // Emily <3
+  atexit([]{ cerr << "Time: " << (ld)clock() / CLOCKS_PER_SEC << nl; });
+  cin.tie(0)->sync_with_stdio(0);
+  cout << fixed << setprecision(12);
+  code();
+  return 0;
+}
+
