a0c7c88366db74e3c7edf1c056d76db51ade4c4b
Victor Chen <chen1999@students.cs.ubc.ca>
2021-10-28 23:15:49 -0700

    euler walk; modified hashing

--- strings/hash.cpp
+++ strings/hash.cpp
@@ -1,30 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std; inline void fileio(const char* in, const char* out) { freopen(in, "r", stdin); freopen(out, "w", stdout); } 
-#define TD typedef
-#define forx(i,n) for(int i = 0; i < (n); ++i)
-#define forxr(i,n) for(int i = (n)-1; i >= 0; --i)
-TD long long ll; TD long double ld; TD pair<int,int> pii; TD pair<ll,ll> pll; TD vector<int> VI; TD vector<bool> VB; TD vector<ll> VL;
-const char nl = '\n'; const int INF = 0x3f3f3f3f; const ll LINF = 0x3f3f3f3f3f3f3f3fll; const ld EPS = 1e-9, PI = acos(-1);
-#define ff first
-#define ss second
-
-class StringHash { const int m; VI p; public:
-  StringHash(int pr = 53, int m = 998802743, int lim = 1e6+50) : m(m), p(1, pr) {
-    do { p.push_back(p.back() * pr % m); } while (p.back() != pr && p.size() < lim); }
-  ll operator()(string const& s) { ll v = 0; forx(i,s.size()) { v = (v + (s[i] - 'a' + 1) * p[i]) % m; } return v; }
+template <size_t N, typename V>
+struct RollingHash {
+  static int toi(char c) { return c-'a'+1; }
+  V pw[N], px[N]; int j=1;
+  RollingHash() {
+    pw[0] = 1; rep(i,1,N) pw[i] = pw[i-1]*31;
+  }
+  void append(char c) {
+    px[j] = px[j-1] + toi(c)*pw[j-1];
+    ++j;
+  }
+  template <typename iter>
+  void append(iter a, iter b) {
+    while(a!=b) append(*a++);
+  }
+  V operator()(int l, int r) const {
+    return (px[r] - px[l])*pw[N-l-1];
+  }
 };
-
-int main() {
-  atexit([](){ cerr << "Time: " << (ld)clock() / CLOCKS_PER_SEC << nl; });
-  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  // Emily <3
-  StringHash hasher;
-
-  string a = "helloworldsapaoke";
-  string b = "hflloworldsapapke";
-  cout << hasher(a) << nl;
-  cout << hasher(b) << nl;
-
-  return 0;
-}
 
