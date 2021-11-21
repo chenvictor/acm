584b28cf2480aec1de9b59d3fc185619629f40d7
BuildTools <victor.chen@alumni.ubc.ca>
2021-11-20 20:03:32 -0800

    fix dijkstra

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,8 +1,18 @@
 // Single Source Shortest Path, no negative weights
 // s = source node
-// d = distances
-void dijkstra(Graph& graph, int s, VL& d, int wIdx=0) {
-  d.assign(graph.N(), LINF); d[s] = 0; VB seen(graph.N(), false); priority_queue<pii, vector<pii>, less<pii>> pq;
-  pq.push({0, s}); int v; while(!pq.empty()) { s = pq.top().second; pq.pop(); if (seen[s]) continue;; seen[s] = true;
-  for (Edge e : graph.getAdjEdge(s)) { v = e.nodeFrom(s); if (d[v] > d[s] + e[wIdx]) { d[v] = d[s] + e[wIdx]; pq.push({d[v], v}); } } }
+template <typename T, typename T2=T>
+vector<T2> dijkstra(const vector<vector<pair<int,T>>>& adj, int s=0) {
+  using V = pair<T2,int>; vector<T2> res(sz(adj), -1); vector<bool> vis(sz(adj));
+  heap<V,greater<V>> pq; pq.emplace(0, s);
+  while(pq.size()) {
+    auto [d, v] = pq.top(); pq.pop();
+    if (vis[v]) continue;
+    vis[v] = 1;
+    res[v] = d;
+    for (auto [u, e] : adj[v]) {
+      pq.emplace(d+e,u);
+    }
+  }
+  return res;
 }
+
