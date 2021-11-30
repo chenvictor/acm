756729dd9165647f0c0f08652f2c708afb448972
BuildTools <victor.chen@alumni.ubc.ca>
2021-11-29 20:14:48 -0800

    re-organize segtree

--- ds/seg/base.cpp
+++ ds/seg/base.cpp
@@ -1,33 +1,38 @@
-template <typename T, int K=17>
+template <typename T, int K>
 struct segtree {
 #define IN  (l <= s && e <= r)
 #define OUT (r <= s || e <= l)
-#define MID ((s+e)/2)
+#define MID (s+(e-s)/2)
+#define LEN (e-s)
 #define L (i*2)
 #define R (i*2+1)
   constexpr static int N = 1 << K;
   array<T,2*N> t;
   T& operator[](int i) { return t[i+N]; }
   void build(int i=1, int s=0, int e=N) {
-    if (s+1==e) return;
+    if (LEN==1) return;
     build(L, s, MID);
     build(R, MID, e);
-    t[i].merge(t[L], t[R]);
+    t[i].merge(t[L],t[R]);
+  }
+  void flush(int i=1, int s=0, int e=N) {
+    if (LEN==1) return;
+    t[i].prop(t[L],t[R],s,e);
+    flush(L,s,MID);
+    flush(R,MID,e);
   }
   T query(int l, int r, int i=1, int s=0, int e=N) {
-    if (IN) return t[i];
-    if (OUT) return T();
-    t[i].prop(t[L], t[R], e-s);
+    if (IN) return t[i]; else if (OUT) return T();
+    t[i].prop(t[L],t[R],s,e);
     return T(query(l, r, L, s, MID), query(l, r, R, MID, e));
   }
   template<typename Op, typename...A>
   void upd(int l, int r, int i, int s, int e, A...args) {
-    if (OUT) return;
-    if (IN && Op::calc(t[i], args..., e-s)) return;
-    t[i].prop(t[L], t[R], e-s);
+    if (OUT || (IN && Op::calc(t[i],args...,s,e))) return;
+    t[i].prop(t[L],t[R],s,e);
     upd<Op>(l, r, L, s, MID, args...);
     upd<Op>(l, r, R, MID, e, args...);
-    t[i].merge(t[L], t[R]);
+    t[i].merge(t[L],t[R]);
   }
   template<typename Op, typename...A>
   void update(int l, int r, A...args){ upd<Op>(l,r,1,0,N,args...); }
@@ -37,8 +42,8 @@ struct segtree {
     if (e <= l) return {N, T()};
     T cur(acc, t[i]);
     const bool ok = Find::ok(cur, args...);
-    if (!ok || s+1==e) return {ok ? s : N, cur};
-    t[i].prop(t[L], t[R], e-s);
+    if (!ok || LEN==1) return {ok ? s : N, cur};
+    t[i].prop(t[L],t[R],s,e);
     auto left = find<Find>(l, L, s, MID, acc, args...);
     if (Find::ok(left.ss, args...)) return left;
     return find<Find>(l, R, MID, e, left.ss, args...);
--- ds/seg/data.cpp
+++ ds/seg/data.cpp
@@ -1,18 +1,19 @@
 struct Data {
+  Data(int val) { }
   /* default null node value USED IN QUERIES so initialize!!! */
   Data() { }
-  Data(int val) { }
+  /* merge logic */
   Data(const Data& l, const Data& r) { merge(l,r); }
   void merge(const Data& l, const Data& r) {
   }
   /* update operations => len=1 at leaf, must return true
    * false to recurse to child nodes */
   struct update {
-    static bool calc(Data& t, int len) {
+    static bool calc(Data& t, int s, int e) {
       return true;
     }
   };
-  void prop(Data& l, Data& r, int len) {
+  void prop(Data& l, Data& r, int s, int e) {
     /* optional lazy prop */
   }
   /* search operations => ok return true if condition is satisfied */
