23e78159e522b7969ad6f258b4ada93c5c376dc1
Victor Chen <victor.chen@alumni.ubc.ca>
2021-12-16 23:37:38 -0800

    update lis

--- misc/lis.cpp
+++ misc/lis.cpp
@@ -1,13 +1,10 @@
 namespace lis {
-  void add(vi& lis, int nx) {
-    // upper_bound for non-decreasing
-    // lower_bound for strictly increasing ?? not sure
-    auto pos = upper_bound(all(lis), nx);
-    if (pos == lis.end()) {
-      lis.push_back(nx);
-    } else {
-      *pos = nx;
-    }
+  template <class T> void add(vector<T>& lis, T nx) {
+    /* lower_bound => strictly increasing
+     * upper_bound => non-decreasing */
+    auto pos = lower_bound(all(lis), nx);
+    if (pos == lis.end()) lis.push_back(nx);
+    else *pos = nx;
   }
 }
 
