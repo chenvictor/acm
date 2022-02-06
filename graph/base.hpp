31471a86c4e2e1d6795748baa4f6067bf4ddab81
Victor Chen <victor.chen@alumni.ubc.ca>
2022-02-06 00:52:18 -0800

    fix graph base

--- graph/base.hpp
+++ graph/base.hpp
@@ -1,5 +1,5 @@
 template <size_t N=0, class T=int>
-struct Graph: public arr<T,N> {
+struct Graph: public arr<vector<T>,N> {
   template <bool directed=0, class...Args> void add_edge(int u, int v, Args... args) {
     (*this)[u].emplace_back(v, args...);
     if (directed) return;
